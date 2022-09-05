170735820f5e2016f6b5205df7345bc6e0693213
unknown <herinyhert@gmail.com>
2022-09-04 23:14:33 -0300

    final

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -16,14 +16,14 @@ import {
 //todos los paises
 export function getCountries(){
     return async function(dispatch){
-        let countries = await axios.get("http://localhost:3001/countries",{});
+        let countries = await axios.get("https://db-countries.herokuapp.com/countries",{});
         return dispatch({ type: GET_COUNTRIES, payload:countries.data })}
 };
 
 export function getNameCountries(name){
     return async function(dispatch){
         try {
-            let json = await axios.get("http://localhost:3001/countries?name=" + name,{});
+            let json = await axios.get("http://db-countries.herokuapp.com/countries?name=" + name,{});
             return dispatch({
                 type: GET_NAME_COUNTRIES,
                 payload: json.data
@@ -37,19 +37,19 @@ export function getNameCountries(name){
 
 export function getAllActivities(){
     return async (dispatch) => {
-        var json = await axios.get('http://localhost:3001/activities')
+        var json = await axios.get('http://db-countries.herokuapp.com/activities')
        return dispatch({ type: GET_ALL_ACTIVITIES, payload: json.data})}
 }
 
 export function datailCountries(payload){
     return async function(dispatch){
-        var json= await axios.get(`http://localhost:3001/countries/${payload}`);
+        var json= await axios.get(`https://db-countries.herokuapp.com/countries/${payload}`);
         return dispatch({ type: DETAIL_COUNTRIES, payload: json.data })}
 }
 
 export function createActivity(payload){
     return async function(){
-        let json = await axios.post("http://localhost:3001/activities", payload);
+        let json = await axios.post("http://db-countries.herokuapp.com/activities", payload);
         return ({ type: CREATE_ACTIVITY, payload: json.data })
     }
 }
